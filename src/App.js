8e4eb21a4ecca4a7bad3df33c7b11d50d19f8e9a
YukiarC <yuukihagino@gmail.com>
2022-03-05 19:06:28 +0900

    first.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Basic1 from './components/Basic1'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Basic1 />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Basic1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Basic1 = () => {
+  return (
+    <div>
+        <h1>React Basic h1</h1>
+        <p>リアクトのベーシック１</p>
+    </div>
+  )
+}
+
+export default Basic1
